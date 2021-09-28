4b398910c93c6d17d4eb021141a9fcbfd2145450
Leo Ackerman <leo.ackerman@mollie.com>
2021-09-28 18:36:45 +0200

    Resolves loblaw-sre/backstage-plugin-gitlab#14

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,3 +1,4 @@
+// @ts-ignore TS1259: Module can only be default-imported using the 'esModuleInterop' flag
 import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
 import { gitlabPlugin, EntityGitlabContent } from '../src/plugin';
