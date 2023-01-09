0bbf949314cf39e3344505fcaf9426a660aa0d2b
Paolo Ferracchiati <paolo.ferracchiati@immobiliare.it>
2023-01-09 12:28:23 +0100

    feat: added bottom links for cards and links in avatars

--- src/components/widgets/PeopleCard/components/PeopleList/PeopleList.tsx
+++ src/components/widgets/PeopleCard/components/PeopleList/PeopleList.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
+import { Link } from '@backstage/core-components';
 import { Person } from '../Person';
-import { PersonData } from '../../../../types';
+import { PersonData, PeopleLink } from '../../../../types';
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowIcon from '@material-ui/icons/ArrowForward';
 
-export const PeopleList = ({ peopleObj }: any) => {
-    const data = peopleObj.data.map((person: PersonData) => {
-        return {
-            name: person.name,
-            email: person.email,
-            avatar_url: person.avatar_url,
-        };
-    });
+interface PeopleListProps {
+    peopleObj: PersonData[];
+    deepLink?: PeopleLink;
+}
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        color: 'inherit',
+        textDecoration: 'none',
+        marginRight: theme.spacing(1),
+    },
+    box: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export const PeopleList = ({ peopleObj, deepLink }: PeopleListProps) => {
+    const classes = useStyles();
     return (
-        <Grid container spacing={1} justifyContent="flex-start">
-            {data.map((person: PersonData) => (
-                <Grid key={person.name} item>
-                    <Person person={person} />
+        <>
+            <Grid container spacing={1} justifyContent="flex-start">
+                {peopleObj.map((person: PersonData) => (
+                    <Grid key={person.name} item>
+                        <Person person={person} />
+                    </Grid>
+                ))}
+            </Grid>
+            {deepLink && (
+                <Grid container spacing={1} justifyContent="flex-end">
+                    <Box
+                        className={classes.box}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        <Link
+                            className={classes.link}
+                            to={deepLink.link}
+                            onClick={deepLink.onClick}
+                        >
+                            {deepLink.title}
+                        </Link>
+                        <ArrowIcon fontSize="small"></ArrowIcon>
+                    </Box>
                 </Grid>
-            ))}
-        </Grid>
+            )}
+        </>
     );
 };
