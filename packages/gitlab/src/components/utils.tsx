bfce13b0eed2b4a42a1b68d62906074f71628d92
maple <75726489+JWWilks@users.noreply.github.com.>
2024-07-12 09:32:30 +0100

    feat(readme): ✨ adds partial support for GLFM

--- packages/gitlab/src/components/utils.tsx
+++ packages/gitlab/src/components/utils.tsx
@@ -114,3 +114,17 @@ const parseCodeOwnerLine = (rule: string): FileOwnership => {
 // ensures that only the following patterns are allowed @octocat @octocat/kitty docs@example.com
 const codeOwnerRegex =
     /(^@[a-zA-Z0-9_\-/]*$)|(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+// Remark does not fully support GLFM, but remark-toc can generate a TOC, but it requires a # heading, whereas GLFM does not.
+export const parseGitLabReadme = (readme: string): string => {
+    const lines = readme.split('\n');
+
+    const modifiedLines = lines.map((line) => {
+        if (/^\[TOC\]|\[\[_TOC_\]\]$/.test(line.trim())) {
+            return `## <!-- injected_toc -->`; // remark-toc turns this into a TOC but keeps the heading, then remark-remove-comments makes the heading invisible
+        }
+        return line;
+    });
+
+    return modifiedLines.join('\n');
+};
